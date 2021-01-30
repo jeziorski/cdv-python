edf5dac3dc3eef22b54dcd2492040c487ba02821
jeziorski <48910840+jeziorski@users.noreply.github.com>
2021-01-30 20:35:56 +0100

    Add files via upload

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import Flask, render_template
 from models import db
+from flask import request
 
 
 app = Flask(__name__)
@@ -8,8 +9,10 @@ app = Flask(__name__)
 def main():
     return render_template('index.html')
 
-@app.route('/showSignUp')
+@app.route('/showSignUp', methods=["GET", "POST"])
 def ShowSignUp():
+    if request.form:
+        print(request.form)
     return render_template('signup.html')
 
 if __name__ == "__main__":
