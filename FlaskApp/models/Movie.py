f0740b656eb5b2950d9c32a626fda3046eb02708
Łukasz <aadamczak@edu.cdv.pl>
2021-01-31 19:40:32 +0100

    updated models

--- FlaskApp/models/Movie.py
+++ FlaskApp/models/Movie.py
@@ -4,8 +4,10 @@ from datetime import datetime
 
 class Movie(db.Model):
     __tablename__ = 'movie'
-    id = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(256), unique=True)
+    date = db.Column(db.DateTime, default=datetime.now)
+    start_time = db.Column(db.Time(),nullable=False)
     duration_in_min = db.Column(db.Integer, unique=False, nullable=False)
+    seats = db.Column(db.Integer)
     director = db.Column(db.String(256), unique=False, nullable=False)
-    year = db.Column(db.DateTime, default=datetime.now)
--- FlaskApp/models/Reservation.py
+++ FlaskApp/models/Reservation.py
@@ -1,12 +1,13 @@
 from . import db
 from datetime import datetime
 from sqlalchemy import Table, Column, Integer, ForeignKey
-from sqlalchemy.orm import relationship
-from sqlalchemy.ext.declarative import declarative_base
 
 class Reservation(db.Model):
     __tablename__ = 'reservation'
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, ForeignKey('User.id'))
     movie_id = db.Column(db.Integer, ForeignKey('Movie.id'))
-    res_date = db.Column(db.DateTime, default=datetime.now)
+    user_id = db.Column(db.Integer, ForeignKey('User.id'))
+    email = db.Column(db.String(256), unique=True, nullable=False)
+    tickets = db.Column(db.Integer, unique=False, nullable=False)
+
+
